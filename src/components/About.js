0fda1236f57ec974c141fb89ea9a0de82133bc72
hassan-abdi269 <honze2030@gamail.com>
2025-04-15 13:39:28 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data";  // Import the image from your data file
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        Hello! I'm a passionate web developer, constantly learning new skills, 
+        and working on exciting projects. I love solving problems with code and creating impactful solutions.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
